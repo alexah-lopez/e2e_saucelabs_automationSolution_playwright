f79f68f912c01dd7ecc0cbc453eb77a5f13fa5e2
Alexahls <alexaisc@hotmail.com>
2025-02-10 12:29:53 -0600

    format checkers installed

--- README.md
+++ README.md
@@ -1,36 +1,42 @@
 <!-- ABOUT THE PROJECT -->
+
 ## About The Project
-Saucedemo is the simple app created to help to the QAE to practice automation skills.
+
+Saucedemo is a simple app created to help to the QAE to practice automation skills.
 
 <!-- ABOUT THE AUTOMATION -->
+
 ## Automated Solution
 
-
 ### Built With
-Frameworks and libraries used:
-* NPM
-* Playwright
 
+Frameworks and libraries used:
+
+- NPM - @10.7.0
+- Playwright -
 
 Programing language:
-* JavaScript
-	
-	 
+
+- JavaScript
+
 <!-- GETTING STARTED -->
+
 ## Getting Started
-	
+
 ### Prerequisites
-- [x] An IDE installed  (Recommended: Visual studio code) 
+
+- [x] An IDE installed (Recommended: Visual studio code)
 - [x] NPM installed
-- [x] Chrome, FireFox and Safari browsers installed 
-	
+- [x] Chrome, FireFox and Safari browsers installed
+
 ### Installation
-- Clone the repository : https://github.com/AlexaLopez-S/e2e_saucelabs_automationSolution_playwright 
+
+- Clone the repository : https://github.com/AlexaLopez-S/e2e_saucelabs_automationSolution_playwright
 
 - Install the following:
-    - npm install  dotenv  --save-dev
+
+    - npm install dotenv --save-dev
 
 - Add a .env file in the env folder with the following information:
-   - USERNAME=""
-   - PASSWORD=""
-
+    - USERNAME=""
+    - PASSWORD=""
--- src/CartPage.js
+++ src/CartPage.js
@@ -1,38 +1,43 @@
-class CartPage{
+class CartPage {
     constructor(page) {
-        this.productName = page.getByTestId("inventory-item-name");
-        this.productDescription = page.getByTestId("inventory-item-desc");
-        this.productPrice = page.getByTestId("inventory-item-price");
-        this.numberOfProductsinCart = page.getByTestId("shopping-cart-badge");
-        this.checkoutButton = page.getByTestId("checkout");
+        this.productName = page.getByTestId('inventory-item-name');
+        this.productDescription = page.getByTestId('inventory-item-desc');
+        this.productPrice = page.getByTestId('inventory-item-price');
+        this.numberOfProductsinCart = page.getByTestId('shopping-cart-badge');
+        this.checkoutButton = page.getByTestId('checkout');
     }
 
-  /**
-   * Get the number of products in the cart
-   * @return {Int}  - number of products in the Cart
-   */
-    async getItemsInCartNumber(){
+    /**
+     * Get the number of products in the cart
+     * @return {Int}  - number of products in the Cart
+     */
+    async getItemsInCartNumber() {
         return await this.numberOfProductsinCart.innerText();
     }
 
-   /**
-   * Get the properties of the product in the cart
-   * @return {Array}  - product properties
-   * Name, Description and Price
-   */
-    async getProductInformation(){
+    async eslintFormat(x, y) {
+        let r = x + y;
+        let string = 'text';
+    }
+
+    /**
+     * Get the properties of the product in the cart
+     * @return {Array}  - product properties
+     * Name, Description and Price
+     */
+    async getProductInformation() {
         const cartProductInformation = [
-          await this.productName.innerText(),
-          await this.productDescription.innerText(),
-          await this.productPrice.innerText()
-        ]
+            await this.productName.innerText(),
+            await this.productDescription.innerText(),
+            await this.productPrice.innerText()
+        ];
         return cartProductInformation;
     }
 
-   /**
-   * Click on the Checkout button
-   */
-    async clickCheckoutButton(){
+    /**
+     * Click on the Checkout button
+     */
+    async clickCheckoutButton() {
         await this.checkoutButton.click();
     }
 }
