83b603776dfdd926d457f8f0e56e48821fd10272
Alexahls <alexaisc@hotmail.com>
2025-02-10 12:39:21 -0600

    updating naming conv

--- src/CatalogPage.js
+++ src/CatalogPage.js
@@ -1,123 +1,121 @@
-import Utils from "./helpers/utils";
+import Utils from './helpers/Utils';
 /**
  * This class obtains the elements in the DOM for the
  * catalog page using different locators
  */
 class CatalogPage {
-  constructor(page) {
-    this.productTitle = page.getByTestId("title");
-    this.productPrices = page.getByTestId("inventory-item-price");
-    this.orderByDropDown = page.locator("//select[@data-test='product-sort-container']");
-    this.addButton = page.locator("//button[text()='Add to cart']");
-    this.productName = page.getByTestId("inventory-item-name");
-    this.productDescription = page.getByTestId("inventory-item-desc");
-    this.productPrice = page.getByTestId("inventory-item-price");
-  }
-
-   /**
-   * Obtaining the price for all the products and adding them 
-   * into an array, removing the $ symbol and converting the data 
-   * to float values
-   * @return {Array} - priceItemsArray
-   */
-  async getPricesList(){
-    const priceItemsList = [];
-    for(let item of await this.productPrices.all()){
-      const price  = await item.innerText();
-      const priceFloat = parseFloat(price.replace("$", ""));
-      priceItemsList.push(priceFloat);
-    }
-    return priceItemsList;
-  }
-
- /**
-   * Order by ascending the price list 
-   * @return {Array} - priceItemOrdered
-   */
-  async orderArrayAscending(itemsPriceList){
-    let orderedList = itemsPriceList.sort(function (a,b) {
-      return a - b; // Ascending
-  });
-  return orderedList;
-  }
-
-   /**
-   * Order by descending the price list 
-   * @return {Array} - priceItemOrdered
-   */
-   async orderArrayDescending(itemsPriceList){
-    let orderedList = itemsPriceList.sort(function (a,b) {
-      return b - a; // Descending
-  });
-  return orderedList;
-  }
-
- /**
-   * Select order by option in the DOM
-   * @param {String} orderByoption - Order by option value
-   */
-  async selectOrderBy(orderByoption){
-    switch(orderByoption)
-    {
-     case "lohi":
-      await this.orderByDropDown.selectOption('Price (low to high)');
-      break;
-
-     case "hilo":
-      await this.orderByDropDown.selectOption('Price (high to low)');
-      break;
-    }
-  }
-
- /**
-   * Comparing two price lists.
-   * @param {Array} priceList1 - Current price list
-   * @param {Array} priceList2 - Price list ordered
-   * @return {Boolean}  - result of comparison
-   */
-  compareThePriceLists(priceList1 , priceList2){
-    const utils = new Utils();
-    return utils.compareTwoArrays(priceList1,priceList2);
-  }
-
-   /**
-   * Add random number of products to the cart
-   * by clicking on Add to cart button
-   * @param {Int} productRandomNumber - Number of products to add
-   */
-  async addRandomProductNumberToCart(productRandomNumber){
-      for(var item = productRandomNumber-1; item >= 0; item --){
-        await this.addButton.nth(item).click();
-      }
+    constructor(page) {
+        this.productTitle = page.getByTestId('title');
+        this.productPrices = page.getByTestId('inventory-item-price');
+        this.orderByDropDown = page.locator("//select[@data-test='product-sort-container']");
+        this.addButton = page.locator("//button[text()='Add to cart']");
+        this.productName = page.getByTestId('inventory-item-name');
+        this.productDescription = page.getByTestId('inventory-item-desc');
+        this.productPrice = page.getByTestId('inventory-item-price');
     }
 
-     /**
-   * Get a random number from 0 to the total number of product 
-   * displayed in the catalog
-   * @return {Int}  - Random number generated
-   */
-    async getRandomProductNumber(){
-      const productsTotal = await this.addButton.count();
-      const utils = new Utils();
-      return utils.generateARandomNumber(productsTotal);
+    /**
+     * Obtaining the price for all the products and adding them
+     * into an array, removing the $ symbol and converting the data
+     * to float values
+     * @return {Array} - priceItemsArray
+     */
+    async getPricesList() {
+        const priceItemsList = [];
+        for (let item of await this.productPrices.all()) {
+            const price = await item.innerText();
+            const priceFloat = parseFloat(price.replace('$', ''));
+            priceItemsList.push(priceFloat);
+        }
+        return priceItemsList;
     }
 
-  /**
-   * Add one random product to the cart
-   * by clicking on Add to cart button
-   * @param {Int} productRandomNumber - Product random number
-   * @return {Array}  - Product properties 
-   * Name, Description and Price
-   */
-  async addARandomProductToCart(productRandomNumber){
-    const itemProperties = [
-      await this.productName.nth(productRandomNumber).innerText(),
-      await this.productDescription.nth(productRandomNumber).innerText(),
-      await this.productPrice.nth(productRandomNumber).innerText()
-    ] 
-    await this.addButton.nth(productRandomNumber).click();
-    return itemProperties;
-  }
-  
+    /**
+     * Order by ascending the price list
+     * @return {Array} - priceItemOrdered
+     */
+    async orderArrayAscending(itemsPriceList) {
+        let orderedList = itemsPriceList.sort(function (a, b) {
+            return a - b; // Ascending
+        });
+        return orderedList;
+    }
+
+    /**
+     * Order by descending the price list
+     * @return {Array} - priceItemOrdered
+     */
+    async orderArrayDescending(itemsPriceList) {
+        let orderedList = itemsPriceList.sort(function (a, b) {
+            return b - a; // Descending
+        });
+        return orderedList;
+    }
+
+    /**
+     * Select order by option in the DOM
+     * @param {String} orderByoption - Order by option value
+     */
+    async selectOrderBy(orderByoption) {
+        switch (orderByoption) {
+            case 'lohi':
+                await this.orderByDropDown.selectOption('Price (low to high)');
+                break;
+
+            case 'hilo':
+                await this.orderByDropDown.selectOption('Price (high to low)');
+                break;
+        }
+    }
+
+    /**
+     * Comparing two price lists.
+     * @param {Array} priceList1 - Current price list
+     * @param {Array} priceList2 - Price list ordered
+     * @return {Boolean}  - result of comparison
+     */
+    compareThePriceLists(priceList1, priceList2) {
+        const utils = new Utils();
+        return utils.compareTwoArrays(priceList1, priceList2);
+    }
+
+    /**
+     * Add random number of products to the cart
+     * by clicking on Add to cart button
+     * @param {Int} productRandomNumber - Number of products to add
+     */
+    async addRandomProductNumberToCart(productRandomNumber) {
+        for (var item = productRandomNumber - 1; item >= 0; item--) {
+            await this.addButton.nth(item).click();
+        }
+    }
+
+    /**
+     * Get a random number from 0 to the total number of product
+     * displayed in the catalog
+     * @return {Int}  - Random number generated
+     */
+    async getRandomProductNumber() {
+        const productsTotal = await this.addButton.count();
+        const utils = new Utils();
+        return utils.generateARandomNumber(productsTotal);
+    }
+
+    /**
+     * Add one random product to the cart
+     * by clicking on Add to cart button
+     * @param {Int} productRandomNumber - Product random number
+     * @return {Array}  - Product properties
+     * Name, Description and Price
+     */
+    async addARandomProductToCart(productRandomNumber) {
+        const itemProperties = [
+            await this.productName.nth(productRandomNumber).innerText(),
+            await this.productDescription.nth(productRandomNumber).innerText(),
+            await this.productPrice.nth(productRandomNumber).innerText()
+        ];
+        await this.addButton.nth(productRandomNumber).click();
+        return itemProperties;
+    }
 }
 export default CatalogPage;
--- tests/cartAndCheckout.spec.js
+++ tests/cartAndCheckout.spec.js
@@ -1,46 +1,44 @@
-import { test, expect } from "@playwright/test";
-import CatalogPage from "../src/CatalogPage";
-import LoginPage from "../src/LoginPage";
-import NavBarPage from "../src/NavBarPage";
-import CartPage from "../src/CartPage";
-import CheckoutYourInformation from "../src/CheckoutYourInformation";
-import CheckoutOverview from "../src/CheckoutOverview";
-import CheckoutComplete from "../src/CheckoutComplete";
+import { test, expect } from '@playwright/test';
+import CatalogPage from '../src/CatalogPage';
+import LoginPage from '../src/LoginPage';
+import NavBarPage from '../src/NavBarPage';
+import CartPage from '../src/CartPage';
+import CheckoutYourInformation from '../src/CheckoutYourInformation';
+import CheckoutOverview from '../src/CheckoutOverview';
+import CheckoutComplete from '../src/CheckoutComplete';
 
 test.beforeEach(async ({ page }) => {
-  console.log(`Running ${test.info().title}`);
-  await page.goto("https://www.saucedemo.com/");
-  const loginPage = new LoginPage(page);
-  const catalogPage = new CatalogPage(page);
-  await loginPage.submitLoginForm(process.env.USERNAME, process.env.PASSWORD);
-  await expect(catalogPage.productTitle).toHaveText("Products");
+    console.log(`Running ${test.info().title}`);
+    await page.goto('https://www.saucedemo.com/');
+    const loginPage = new LoginPage(page);
+    const catalogPage = new CatalogPage(page);
+    await loginPage.submitLoginForm(process.env.USERNAME, process.env.PASSWORD);
+    await expect(catalogPage.productTitle).toHaveText('Products');
 });
 
-
-test("Validate properties of a product added to the cart", async ({ page }) => {
-  const catalogPage = new CatalogPage(page);
-  const navBar = new NavBarPage(page);
-  const cartPage = new CartPage(page);
-  const randomProductNumber = await catalogPage.getRandomProductNumber();
-  const productInformation = await catalogPage.addARandomProductToCart(randomProductNumber);
-  await navBar.clickCartIcon();
-  const cartProductInformation = await cartPage.getProductInformation();
-  expect (catalogPage.compareThePriceLists(productInformation,cartProductInformation)).toBe(true);
+test('Validate properties of a product added to the cart', async ({ page }) => {
+    const catalogPage = new CatalogPage(page);
+    const navBar = new NavBarPage(page);
+    const cartPage = new CartPage(page);
+    const randomProductNumber = await catalogPage.getRandomProductNumber();
+    const productInformation = await catalogPage.addARandomProductToCart(randomProductNumber);
+    await navBar.clickCartIcon();
+    const cartProductInformation = await cartPage.getProductInformation();
+    expect(catalogPage.compareThePriceLists(productInformation, cartProductInformation)).toBe(true);
 });
 
-test("Complete purchase", async ({ page }) => {
-  const catalogPage = new CatalogPage(page);
-  const navBar = new NavBarPage(page);
-  const cartPage = new CartPage(page);
-  const checkoutYourInformation = new CheckoutYourInformation(page);
-  const checkoutOverview = new CheckoutOverview(page);
-  const checkoutComplete = new CheckoutComplete(page);
-  const randomProductNumber = await catalogPage.getRandomProductNumber();
-  await catalogPage.addRandomProductNumberToCart(randomProductNumber);
-  await navBar.clickCartIcon();
-  await cartPage.clickCheckoutButton();
-  await checkoutYourInformation.fillCheckoutForm(process.env.USERNAME, process.env.PASSWORD,process.env.POSTALCODE);  
-  await checkoutOverview.clickFinishButton();
-  expect (await checkoutComplete.getSuccessText()).toBe("Thank you for your order!");
-
+test('Complete purchase', async ({ page }) => {
+    const catalogPage = new CatalogPage(page);
+    const navBar = new NavBarPage(page);
+    const cartPage = new CartPage(page);
+    const checkoutYourInformation = new CheckoutYourInformation(page);
+    const checkoutOverview = new CheckoutOverview(page);
+    const checkoutComplete = new CheckoutComplete(page);
+    const randomProductNumber = await catalogPage.getRandomProductNumber();
+    await catalogPage.addRandomProductNumberToCart(randomProductNumber);
+    await navBar.clickCartIcon();
+    await cartPage.clickCheckoutButton();
+    await checkoutYourInformation.fillCheckoutForm(process.env.USERNAME, process.env.PASSWORD, process.env.POSTALCODE);
+    await checkoutOverview.clickFinishButton();
+    expect(await checkoutComplete.getSuccessText()).toBe('Thank you for your order!');
 });
